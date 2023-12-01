0b7b5ff005778943e3826835c69ba50fa92c54e5
Milosz Krajewski <miloszkrajewski@o2.pl>
2023-12-01 14:48:54 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# K4os.CronEx
+
+[![NuGet Stats](https://img.shields.io/nuget/v/K4os.CronEx.svg)](https://www.nuget.org/packages/K4os.CronEx)
+
+# Usage
+
+TBD
+
+# Build
+
+```shell
+build
+```
---
+++ src/K4os.CronEx.App/Program.cs
@@ -0,0 +1,52 @@
+﻿// ReSharper disable UnusedParameter.Local
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+var app = Host
+	.CreateDefaultBuilder(args)
+	.UseSerilog(
+		(context, configuration) => {
+			var serilogTemplate =
+				"[{Timestamp:HH:mm:ss.fff} {Level:u3}] ({SourceContext}) {Message:lj}{NewLine}{Exception}";
+			configuration
+				.ReadFrom.Configuration(context.Configuration)
+				.Enrich.FromLogContext()
+				.WriteTo.Console(outputTemplate: serilogTemplate);
+		})
+	.Build();
+
+await app.StartAsync();
+
+await Execute(
+	app.Services,
+	app.Services.GetRequiredService<ILoggerFactory>(),
+	app.Services.GetRequiredService<IConfiguration>());
+
+await app.StopAsync();
+return;
+
+//--------------------------------------------------------------------------------------------------
+
+static async Task Execute(
+	IServiceProvider serviceProvider,
+	ILoggerFactory loggerFactory,
+	IConfiguration configuration)
+{
+	await Task.CompletedTask;
+	var log = loggerFactory.CreateLogger("root");
+
+	log.LogTrace("Trace...");
+	log.LogDebug("Debug...");
+	log.LogInformation("Info...");
+	log.LogWarning("Warning...");
+	log.LogError("Error...");
+	log.LogCritical("Critical...");
+
+	await Task.WhenAny(
+		Task.Run(() => Console.In.ReadLineAsync()),
+		Task.Delay(TimeSpan.FromSeconds(5)));
+}
